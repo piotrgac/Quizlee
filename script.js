00edeb572292616c15b2bb0d1641916527852a69
Piotr <piotrgac8@gmail.com>
2022-03-04 16:25:03 +0100

    0.61

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-var Questions = [
-    1 = {
-        pytanie: "Zachodni sąsiad Polski",
-        answera: "Niemcy",
-        answerb: "Włochy",
-        answerc: "Rosja",
-        answerd: "Ukraina",
-    },
-    2 = {
-        pytanie: "Rok chrzestu Polski",
-        answera: "996",
-        answerb: "966",
-        answerc: "699",
-        answerd: "669",
-    },
-]
-
-function getQuestion(){
-    RandomNumber();
-    document.getElementById(pytanie);
+function active_box(){
+    document.getElementById('que1').style.border='2px solid #1A1A1A';
+}
+function disable_box(){
+    document.getElementById('que1').style.border="0px solid #000";
+}
+function check_box(){
+    document.getElementById('answ1').checked="checked";
 }
 
-function RandomNumber(){
-    rand = Math.floor(Math.Random(2+1));
+
+function active_box1(){
+    document.getElementById('que2').style.border='2px solid #1A1A1A';
+}
+function disable_box1(){
+    document.getElementById('que2').style.border="0px solid #000";
+}
+function check_box1(){
+    document.getElementById('answ2').checked="checked";
 }
 
-alert(answera);
+
+function active_box2(){
+    document.getElementById('que3').style.border='2px solid #1A1A1A';
+}
+function disable_box2(){
+    document.getElementById('que3').style.border="0px solid #000";
+}
+function check_box2(){
+    document.getElementById('answ3').checked="checked";
+}
