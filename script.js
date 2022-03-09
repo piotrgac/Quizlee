27c45ab6d7e32dfc0a392769059a417b399c488c
Piotr <piotrgac8@gmail.com>
2022-03-09 22:06:39 +0100

    radio check

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
-function active_box(){
+function getID(clicked_id){
+     console.log(clicked_id);
+     document.getElementById(clicked_id).style.border='2px solid #1A1A1A';
+}
+function disable_box(clicked_id){
+     document.getElementById(clicked_id).style.border="0px solid #000";
+}
+function check_box(answer){
+    document.getElementById(answer).checked="checked";
+}
+/*function active_box(){
     document.getElementById('que1').style.border='2px solid #1A1A1A';
-}
-function disable_box(){
-    document.getElementById('que1').style.border="0px solid #000";
-}
-function check_box(){
-    document.getElementById('answ1').checked="checked";
 }
 
 
 function active_box1(){
-    document.getElementById('que2').style.border='2px solid #1A1A1A';
+    document.getElementById(optionID).style.border='2px solid #1A1A1A';
 }
 function disable_box1(){
-    document.getElementById('que2').style.border="0px solid #000";
+    document.getElementById(optionID).style.border="0px solid #000";
 }
-function check_box1(){
-    document.getElementById('answ2').checked="checked";
-}
-
-
-function active_box2(){
-    document.getElementById('que3').style.border='2px solid #1A1A1A';
-}
-function disable_box2(){
-    document.getElementById('que3').style.border="0px solid #000";
-}
-function check_box2(){
-    document.getElementById('answ3').checked="checked";
-}
+*/
