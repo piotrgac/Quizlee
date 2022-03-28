c2774a4ca1ef5c2f23cfd8820c37d25023e7fb62
Piotr <piotrgac8@gmail.com>
2022-03-28 21:40:45 +0200

    0.70

--- script.js
+++ script.js
@@ -6,14 +6,4 @@ function getID(clicked_id) {
 
 function check_box(answer){
     document.getElementById(answer).checked = "checked";
-    document.getElementById(current_id).style.border = "2px solid black";
-}
-function disable_box(answer){
-    addEventListener(onmouseout,)
-    if (document.getElementById(answer).checked === true){
-        document.getElementById(current_id).style.border="2px solid black";
-    }
-    else {
-        document.getElementById(current_id).style.border="0px solid black";
-    }
 }
